66197b76aaec2accdea354449dba7b6a4f359730
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2023-10-04 21:43:45 +0300

    modules module section

---
+++ starter/modules.js
@@ -0,0 +1,17 @@
+console.log(arguments);
+console.log(require('module').wrapper);
+
+// module.exports
+const Calculator = require('./test-module-1');
+const calc1 = new Calculator();
+console.log(calc1.add(-3, -7));
+
+// exports
+// const calc2 = require('./test-module-2');
+const { add, multiply, divide } = require('./test-module-2');
+console.log(add(-5, 5));
+
+// caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
